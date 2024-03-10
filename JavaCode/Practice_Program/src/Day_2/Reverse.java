19474fb2301fbbcf303b9ac485821f8bede036f6
varadsalokhe99 <salokhevarad99@gmail.com~>
2024-03-11 01:13:31 +0530

    Reverse Number

--- JavaCode/Practice_Program/src/Day_2/Reverse.java
+++ JavaCode/Practice_Program/src/Day_2/Reverse.java
@@ -1,9 +1,19 @@
 package Day_2;
 
+import java.util.Scanner;
+
 public class Reverse {
 
 	public static void main(String[] args) {
-	
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter number:");
+		int n=sc.nextInt();
+		int sum=0;
+		while(n!=0) {
+			sum=sum*10+(n%10);
+			n=n/10;
+		}
+		System.out.println(sum);
 	}
 
 }
